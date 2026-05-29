8c59abca443c995ae9e4ac8b22b6d80853e23af1
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2026-05-29 14:06:48 +0300

    fix chat

--- src/config/gemini.ts
+++ src/config/gemini.ts
@@ -1,16 +1,6 @@
-import { GoogleGenAI, ThinkingLevel } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 
 export const ai = new GoogleGenAI({
-  apiKey: import.meta.env.VITE_REACT_APP_GEMINI_API_KEY,
-});
-export const chat = ai.chats.create({
-  model: "gemini-2.5-flash",
-  history: [],
-  // config: {
-  //   temperature: 0.1,
-  //   thinkingConfig: {
-  //     thinkingLevel: ThinkingLevel.LOW,
-  //   },
-  // },
-});
+  apiKey: import.meta.env.VITE_REACT_APP_GEMINI_API_KEY,});
+
 
--- src/hooks/useGemini.tsx
+++ src/hooks/useGemini.tsx
@@ -1,6 +1,7 @@
 import {useCallback, useState} from "react";
 import type {messageType} from "../types/types.ts";
-import {chat} from "../config/gemini.ts";
+import { ai } from "../config/gemini.ts";
+import {ThinkingLevel} from "@google/genai";
 
 
 export const useGemini = () => {
@@ -12,18 +13,39 @@ export const useGemini = () => {
         setMessages((prevMessage) => [...prevMessage, message]);
     };
 
-    const sendMessage  = useCallback(async (content: string) => {
+    const updateLastMessage = (content: string) => {
+        setMessages((prev) => prev.map((message, index) =>
+            index === prev.length - 1 ? {...message, content} : message
+        ));
+    };
+
+    const sendMessage = useCallback(async (content: string) => {
         setIsLoading(true);
         setError(null);
         try {
+            const chat = ai.chats.create({
+                model: "gemini-2.5-flash",
+                history: [],
+                // config: {
+                //     temperature: 0.1,
+                //     thinkingConfig: {
+                //         thinkingLevel: ThinkingLevel.LOW,
+                //     },
+                // },
+            });
+
             addMessage({content, role: "user"});
+            addMessage({content: '', role: 'assistant'});
+
+            let fullAssistantResponse = '';
+
             const stream = await chat.sendMessageStream({
                 message: content,
             });
 
             for await (const chunk of stream) {
-                console.log(chunk)
-                addMessage({content: chunk.text, role: 'assistant'} );
+                fullAssistantResponse += chunk.text;
+                updateLastMessage(fullAssistantResponse)
 
             }
         } catch (err: unknown) {
@@ -34,5 +56,5 @@ export const useGemini = () => {
         }
     }, [])
 
-    return {isLoading, error, sendMessage, messages };
+    return {isLoading, error, sendMessage, messages};
 };
